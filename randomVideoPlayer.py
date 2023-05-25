9bfa644aeb8b1cf4f1f2f64b1c6e6baf5aa4b65f
KoppelTAR <punanepikne@gmail.com>
2023-05-25 14:05:38 +0300

    Add UI to open a new video

--- randomVideoPlayer.py
+++ randomVideoPlayer.py
@@ -1,8 +1,17 @@
 import os, random
 import webbrowser
+import tkinter as tk
 from decouple import config
 basedir = config('DIRECTORY')
 
-file = random.choice([x for x in os.listdir(basedir) if os.path.isfile(os.path.join(basedir, x))])
 
-webbrowser.open(os.path.join(basedir, file))
+def play_video():
+    file = random.choice([x for x in os.listdir(
+        basedir) if os.path.isfile(os.path.join(basedir, x))])
+    webbrowser.open(os.path.join(basedir, file))
+
+r = tk.Tk()
+r.title('Random video player')
+button = tk.Button(r, text='New video', width=25, command=play_video)
+button.pack()
+r.mainloop()
